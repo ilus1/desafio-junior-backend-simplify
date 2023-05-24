a8ca1b6709c880edfb9b371bfc48e9fbaf0592cb
ilus1 <ilusdasilva@gmail.com>
2023-05-24 15:55:08 -0300

    fix: Adding correct annotation for ManyToOne association

--- simplify.task-manager/src/main/java/com/api/simplify/taskmanager/models/TaskModel.java
+++ simplify.task-manager/src/main/java/com/api/simplify/taskmanager/models/TaskModel.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -32,6 +33,6 @@ public class TaskModel implements Serializable {
 	@Column
 	private Boolean accomplished;
 
-	@Column
+	@ManyToOne
 	private PriorityModel priority;
 }

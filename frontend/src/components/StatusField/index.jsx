4689012e5df931772b8c69d7c4d64f438b66070b
ilus1 <ilusdasilva@gmail.com>
2023-05-25 20:48:52 -0300

    feat: Added task status form component

---
+++ frontend/src/components/StatusField/index.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+
+import { Container, Label, Input } from './styles';
+
+const StatusField = ({ label, value, onChange }) => {
+
+  return (
+    <Container>
+      <Label>{label}</Label>
+      <Input type="checkbox" value={value} onChange={onChange} />
+    </Container>
+  );
+}
+
+StatusField.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+}
+
+export default StatusField;
---
+++ frontend/src/components/StatusField/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  width: 70%;
+  height: 30px;
+  border: none;
+  border-radius: 5px;
+`;
+
+export const Label = styled.label`
+  margin-right: 10px;
+
+`;
+
+export const Input = styled.input`
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;

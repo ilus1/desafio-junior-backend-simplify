e6d709f0059d7b11f9fe060d0f9305005a34b790
ilus1 <ilusdasilva@gmail.com>
2023-05-25 15:59:53 -0300

    fix: removing comment

--- simplify.task-manager/src/main/java/com/api/simplify/taskmanager/CorsConfig.java
+++ simplify.task-manager/src/main/java/com/api/simplify/taskmanager/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Replace with your frontend domain
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
